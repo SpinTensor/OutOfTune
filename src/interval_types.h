5072f7c1a4a5618464e3b531ec62e1d1b28501eb
Felix Uhl <uhl_felix@web.de>
2021-01-24 16:39:57 +0100

    added interval types

---
+++ src/interval_types.h
@@ -0,0 +1,27 @@
+#ifndef INTERVAL_TYPES_H
+#define INTERVAL_TYPES_H
+
+#include "fraction_types.h"
+
+enum interval_ID_e {
+   P1,
+   m2, M2,
+   m3, M3,
+   P4,
+   P5,
+   m6, M6,
+   m7, M7,
+   P8
+};
+
+typedef struct {
+   char *name;
+   enum interval_ID_e ID;
+   int ht_steps;
+   frac_t freqscale;
+   int minpow;
+   int maxpow;
+   int pow;
+} interval_t;
+
+#endif
