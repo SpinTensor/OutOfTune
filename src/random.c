66d3b929304a7d0ac577286fc15173c99a7409ef
Felix Uhl <uhl_felix@web.de>
2021-01-27 20:51:11 +0100

    restructured random number generator

--- src/random.c
+++ src/random.c
@@ -4,9 +4,9 @@
 int next_int_random(int lower, int upper) {
    int n = upper - lower + 1;
    int remainder = RAND_MAX % n;
-   int x = RAND_MAX - remainder;
-   while (x >= RAND_MAX - remainder) {
+   int x;
+   do {
       x = rand();
-   }
+   } while (x >= RAND_MAX - remainder);
    return lower + x % n;
 }
