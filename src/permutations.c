31b9d0fdae76810b4af8b5dbf8822eb80b3d6eb7
Felix Uhl <uhl_felix@web.de>
2021-01-27 20:06:53 +0100

    added permutation routines

---
+++ src/permutations.c
@@ -0,0 +1,14 @@
+#include "random.h"
+
+void swap_elements(int *list, int i, int j) {
+   int tmp = list[i];
+   list[i] = list[j];
+   list[j] = tmp;
+}
+
+void shuffle_list(int n, int *list) {
+   for (int i=n-1; n>0; i++) {
+      int r = next_int_random(1, i);
+      swap_elements(list, i, z);
+   }
+}
---
+++ src/permutations.h
@@ -0,0 +1,8 @@
+#ifndef PERMUTATIONS_H
+#define PERMUTATIONS_H
+
+void swap_elements(int *list, int i, int j);
+
+void shuffle_list(int n, int *list);
+
+#endif
