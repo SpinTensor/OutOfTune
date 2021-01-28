1e621b4bb6e12a064c1f3125f601ebc017706846
Felix Uhl <uhl_felix@web.de>
2021-01-28 19:56:51 +0100

    restructured sequence properties functions

--- src/sequence_properties.h
+++ src/sequence_properties.h
@@ -4,6 +4,6 @@
 #include "sequence_types.h"
 #include "cmd_opt_types.h"
 
-void sequence_properties(sequence_t *sequence, cmd_options_t options);
-
+void sequence_properties(sequence_t *sequence, cmd_options_t options,
+                         int *maxhstepsup, int *maxhstepsdown, int *avoidviolations);
 #endif
