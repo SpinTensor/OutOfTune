e2a56c20fcd72c793023cef3e92e761cf80e43ed
Felix Uhl <uhl_felix@web.de>
2021-01-31 12:26:43 +0100

    bugfix for negative interval powers

--- src/interval_list_properties.c
+++ src/interval_list_properties.c
@@ -15,8 +15,14 @@ int interval_list_halfstep_shift(interval_t *interval_list) {
 void interval_list_freq_scale(interval_t *interval_list, mpq_t *scale) {
    mpq_set_si(*scale, 1, 1);
    for (int i=0; i<nintervals; i++) {
-      for (int ipow=0; ipow<interval_list[i].pow; i++) {
-         mpq_mul(*scale, *scale, interval_list[i].freqscale);
+      if (interval_list[i].pow > 0) {
+         for (int ipow=0; ipow<interval_list[i].pow; ipow++) {
+            mpq_mul(*scale, *scale, interval_list[i].freqscale);
+         }
+      } else if (interval_list[i].pow < 0) {
+         for (int ipow=0; ipow<-interval_list[i].pow; ipow++) {
+            mpq_div(*scale, *scale, interval_list[i].freqscale);
+         }
       }
    }
 }
