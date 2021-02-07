ed8ac7ccd5c29ce96b7e71356662722d67f08b20
Felix Uhl <uhl_felix@web.de>
2021-02-07 09:11:13 +0100

    made optimization more robust

--- src/opt_sequence.c
+++ src/opt_sequence.c
@@ -1,3 +1,5 @@
+#include <stdbool.h>
+
 #include "sequence_types.h"
 #include "cmd_opt_types.h"
 #include "sequence_properties.h"
@@ -7,16 +9,30 @@
 
 void optimize_sequence(sequence_t *sequence_ptr, cmd_options_t options) {
    int length = sequence_ptr->sequence_length;
-   shuffle_list(sequence_ptr->sequence_length, sequence_ptr->interval_sequence);
-   int sum = 0;
-   for (int ielem=0; ielem<length-1; ielem++) {
-      int i = next_int_random(ielem+1, length-1);
-      while (sum + sequence_ptr->interval_sequence[i] > options.maxhstepsup ||
-             sum + sequence_ptr->interval_sequence[i] < -options.maxhstepsdown) {
-         i = next_int_random(ielem+1, length-1);
+   bool success = false;
+   while (!success) {
+      success = true;
+      shuffle_list(sequence_ptr->sequence_length, sequence_ptr->interval_sequence);
+      int sum = 0;
+      for (int ielem=0; ielem<length-1; ielem++) {
+         int i = next_int_random(ielem+1, length-1);
+         int itrials = 0;
+         while (sum + sequence_ptr->interval_sequence[i] > options.maxhstepsup ||
+                sum + sequence_ptr->interval_sequence[i] < -options.maxhstepsdown) {
+            i = next_int_random(ielem+1, length-1);
+            itrials++;
+            if (itrials> 100*length) {
+               success = false;
+               break;
+            }
+         }
+         if (success) {
+            sum += sequence_ptr->interval_sequence[i];
+            swap_elements(sequence_ptr->interval_sequence, ielem, i);
+         } else {
+            break;
+         }
       }
-      sum += sequence_ptr->interval_sequence[i];
-      swap_elements(sequence_ptr->interval_sequence, ielem, i);
    }
    sequence_properties(sequence_ptr,
                        &(sequence_ptr->maxhstepsup),
